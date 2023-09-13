17733a5f95c1ba31e1f7279600870034ef2bb33b
Дмитро <129288908+Kreker00@users.noreply.github.com>
2023-09-14 00:03:32 +0300

    push 02-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+// import VimeoPlayer from '@vimeo/player';
+// let;
+import { throttle } from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCAL_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', function ({ seconds }) {
+  localStorage.setItem(LOCAL_KEY, seconds);
+});
+player.setCurrentTime(localStorage.getItem(LOCAL_KEY) || 0);
